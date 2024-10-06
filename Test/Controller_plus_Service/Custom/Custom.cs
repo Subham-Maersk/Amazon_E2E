ea51383ed61ff01517430648ce3b4924639012a4
Adarsh Kumar <adarsh.k@maersk.com>
2024-10-07 02:51:48 +0530

    international_customs events generation

--- Test/Controller_plus_Service/Custom/Custom.cs
+++ Test/Controller_plus_Service/Custom/Custom.cs
@@ -51,11 +51,30 @@ namespace Amazon_E2E_copy.Tests
         var selectedValue2 = await _page.Locator(dropdownSelector2).InputValueAsync();
         Console.WriteLine($"Selected value2: {selectedValue2}");
          await Task.Delay(1200);
-            await _page.ClickAsync("//button[text()='Create']']");
+            await _page.ClickAsync("//button[text()='Create']");
+    
+            await _page.ClickAsync("//ul[@class='dropbtn icons btn-right showLeft']");
+    
+         var dropdownSelector3 = "//div[@id='myDropdown3339']"; // This is the ID or selector of the <select> element
+
+        await _page.Locator(dropdownSelector3).SelectOptionAsync(new[] { "Insert Overpacks" });
+
+        var selectedValue3 = await _page.Locator(dropdownSelector).InputValueAsync();
+        Console.WriteLine($"Selected value: {selectedValue3}");
+        await Task.Delay(1200);
+await _page.FillAsync("//textarea[@id='dispatch_numbers']","AMSLAXWSSF815XUYXW000001088");
+await _page.ClickAsync("//button[@id='send-dispatches']");
+await _page.ClickAsync("//a[@class='uk-button uk-button-orange uk-button-cancel uk-icon uk-align-right uk-margin-remove-bottom']");
+
             await Task.Delay(5 * 60 * 1000);
+            await _page.ClickAsync("(//button[@class='dt-button'])[1]");
               await Task.Delay(5 * 60 * 1000);
+              await _page.ClickAsync("(//button[@class='dt-button'])[1]");
                 await Task.Delay(5 * 60 * 1000);
+                await _page.ClickAsync("(//button[@class='dt-button'])[1]");
                   await Task.Delay(5 * 60 * 1000);
+                  await _page.ClickAsync("(//button[@class='dt-button'])[1]");
+                  await _page.IsEnabledAsync("//span[text()='All documents generated']");
 
         }
 
