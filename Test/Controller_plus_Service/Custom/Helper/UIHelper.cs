269a5a884b05f8a355ff0fcfca5c941a8642321f
Adarsh Kumar <adarsh.k@maersk.com>
2024-10-08 01:18:45 +0530

    page object model and allure reporting

---
+++ Test/Controller_plus_Service/Custom/Helper/UIHelper.cs
@@ -0,0 +1,48 @@
+using Microsoft.Playwright;
+using System.Threading.Tasks;
+
+namespace TestProject.Helpers
+{
+    public class UIHelper
+    {
+        private IPage _page;
+
+        public UIHelper(IPage page)
+        {
+            _page = page;
+        }
+
+        // text field 
+        public async Task FillTextField(string selector, string value)
+        {
+            var element = _page.Locator(selector);
+            await element.FillAsync(value); 
+        }
+
+        //dropdown text
+        public async Task SelectDropdownByText(string selector, string value)
+        {
+            var element = _page.Locator(selector);
+            await element.SelectOptionAsync(new SelectOptionValue { Label = value });  
+        }
+
+        // dropdown value
+        public async Task SelectDropdownByValue(string selector, string value)
+        {
+            var element = _page.Locator(selector);
+            await element.SelectOptionAsync(value);  
+        }
+
+        // click
+        public async Task ClickButton(string selector)
+        {
+            var button = _page.Locator(selector);
+            await button.ClickAsync(); 
+        }
+
+        public async Task TakeScreenshotAsync(string filePath)
+        {
+            await _page.ScreenshotAsync(new PageScreenshotOptions { Path = filePath });
+        }
+    }
+}
